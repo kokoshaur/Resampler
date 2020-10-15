439134f0ccf5d5ce963978ce8bc87640aec6740a
Kokoshaur <kokoshaur-10@mail.ru>
2020-10-15 12:46:40 +0300

    Jopa

--- Resampler/main.cpp
+++ Resampler/main.cpp
@@ -4,8 +4,17 @@
 #include <fstream>
 
 int main() {
-    BMPresizer a("..\\1.bmp", "..\\res.bmp");
+    BMPresizer a("..\\123.bmp", "..\\res.bmp");
     a.resize(2, 2);
+
+    BMPresizer a1("..\\123.bmp", "..\\res2.bmp");
+    a1.resize(0.5, 0.5);
+
+    BMPresizer a2("..\\123.bmp", "..\\res4.bmp");
+    a2.resize(0.5, 2);
+
+    BMPresizer a3("..\\123.bmp", "..\\res5.bmp");
+    a3.resize(2, 0.5);
 
     system("pause");
     return 0;
